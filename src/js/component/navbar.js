b43ae7bbf6af7600ec00e5f88848d5ccb8364cf0
ManuelCebreiro <cebreirom@gmail.com>
2022-07-20 19:07:02 +0000

    cambios

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,14 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+	const img ="https://i.pinimg.com/originals/b6/af/5a/b6af5aeff0ee43a4253fc70c167bb6db.png"
 	return (
 		<nav className="navbar navbar-light bg-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+			<img src={img}
+			width="75 px"
+			height="75 px"></img>
+
 			</Link>
 			<div className="ml-auto">
 				<Link to="/Demo">
-					<button className="btn btn-primary">Personajes descripción</button>
+					<button className="btn btn-primary">"</button>
 				</Link>
 			</div>
 		</nav>
